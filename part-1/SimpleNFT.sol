4a50f9c96c80194e67d9765996fbb7ffc1a4d14a
Hamisi Ngao <newhamisi@gmail.com>
2022-06-22 23:26:23 +0300

    Implemtent function to return tokenURI for each tokenId

--- part-1/SimpleNFT.sol
+++ part-1/SimpleNFT.sol
@@ -1,22 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract SimpleNft {
+    using Strings for uint256;
+
     mapping(uint256 => address) private _owners;
+    string public baseURL = "https://example.com/images/";
 
     function mint(uint256 _tokenId) external {
-        require(_owners[_tokenId] == address(0) , "already minted");
-        require(_tokenId < 100 , "token id too large");
+        require(_owners[_tokenId] == address(0), "already minted");
+        require(_tokenId < 100, "token id too large");
         _owners[_tokenId] = msg.sender;
-
     }
-    
+
     function ownerOf(uint256 _tokenId) external view returns (address) {
-        require( _owners[_tokenId] != address(0), "No such token");
+        require(_owners[_tokenId] != address(0), "No such token");
         return _owners[_tokenId];
     }
 
-    function transferFrom(address _from, address _to, uint256 _tokenId) external {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external {
         // check if token exists
         require(_owners[_tokenId] != address(0), "No such token");
         require(_owners[_tokenId] == _from, "cannot transfer from");
@@ -24,5 +31,8 @@ contract SimpleNft {
         _owners[_tokenId] = _to;
     }
 
-
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        require(_owners[_tokenId] != address(0), "No such token");
+        return string(abi.encodePacked(baseURL, _tokenId.toString()));
+    }
 }
